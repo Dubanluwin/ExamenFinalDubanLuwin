747f5b409f2b73e831550b68d2a0f7b97dda1b2d
Duban Luwin <dubanluwin@gmail.com>
2025-06-12 16:26:21 +0200

    Primer push para subir el proyecto a GitHub.

---
+++ src/main/java/com/example/ExamenFinalDubanLuwin/ExamenFinalDubanLuwinApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamenFinalDubanLuwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalDubanLuwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalDubanLuwinApplication.class, args);
+	}
+
+}
